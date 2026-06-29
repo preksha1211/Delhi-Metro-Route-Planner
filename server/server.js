c72d52442b124a781ad8912e424da29c2bf1b724
Preksha Gupta <prekshag460@gmail.com>
2026-06-29 07:46:29 +0530

    Initial MERN setup with MongoDB connection

--- server/server.js
+++ server/server.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+// Connect Database
+connectDB();
 
 const app = express();
 
@@ -12,8 +19,8 @@ app.get("/", (req, res) => {
     res.send("🚇 Delhi Metro API is Running...");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`🚀 Server is running on http://localhost:${PORT}`);
 });
